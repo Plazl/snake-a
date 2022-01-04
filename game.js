779b3a436da0374ae8a383bb695ae7c163624918
jhnbg rth <I1234itsplaz@outlook.com>
2022-01-04 12:59:47 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,16 @@
+var x = 300
+var y = 273.5
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.fillStyle = "#00FF00";
+ctx.fillRect(x, y, 15, 15);
+game.addEventListener("keypress" changeX)
 
+game.addEventListener("keypress" changeY)
+function changeX() {
+ x = x - 5;
+}
+
+function changeY() {
+ y = y - 5;
+}
